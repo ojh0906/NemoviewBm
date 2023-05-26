4a848f33c8d6981dbaf7fdded90e5c0f6c124e50
jamie <woqja0@gmail.com>
2023-05-26 19:31:18 +0900

    jamie update 2023.05.26 ::: 알림 설정

--- src/pages/mypage/AlramSetting.vue
+++ src/pages/mypage/AlramSetting.vue
@@ -15,12 +15,12 @@
           <div class="alarm-line">
             <p class="title">모바일 알림</p>
             <p class="txt">포인트 소진 및 광고 현황, 업데이트, 이벤트 등의 소식을 휴대폰 번호로 받을 수 있습니다.</p>
-            <img src="/image/mypage/switch-on.png">
+            <img :src="this.info_mobile ? '/image/mypage/switch-on.png':'/image/mypage/switch-off.png'" @click="this.info_mobile = !this.info_mobile; modifyMember(1)">
           </div>
           <div class="alarm-line">
             <p class="title">이메일 알림</p>
             <p class="txt">포인트 소진 및 광고 현황, 업데이트, 이벤트 등의 소식을 이메일로 받을 수 있습니다.</p>
-            <img src="/image/mypage/switch-off.png">
+            <img :src="this.info_email ? '/image/mypage/switch-on.png':'/image/mypage/switch-off.png'" @click="this.info_email = !this.info_email; modifyMember(2)">
           </div>
         </div>
       </div>
@@ -44,43 +44,53 @@ export default {
   props: [''],
   data() {
     return {
-      password:'',
+      info_mobile:false,
+      info_email:false,
     }
   },
   watch: {
 
   },
   methods: {
-    showPassword() {
-      var x = document.getElementById("password");
-      if (x.type === "password") {
-        x.type = "text";
-      } else {
-        x.type = "password";
-      }
-    },
-    modifyMember(){
-      if(this.password === ''){ alert('비밀번호를 입력해주세요.'); return;}
-
+    modifyMember(type){
       let param = {
-        password: this.password,
+
       }
-      http.put("/member/"+this.getLoginMember.member+"/password", param).then((response) => {
+      if(type === 1){
+        param.info_mobile = this.info_mobile;
+      } else {
+        param.info_email = this.info_email;
+      }
+      http.put("/member/"+this.getLoginMember.member+"/info", param).then((response) => {
         if (response.data.CODE == 200) {
-          alert('비밀번호가 변경되었습니다.');
-          this.$store
-              .dispatch("logout", {})
-              .then(() => {this.$router.push('/login')})
-              .catch(({ message }) => alert(message))
+          this.getMember();
         } else {
           alert('시스템문제 발생. 관리자에게 문의하세요.');
         }
       }).catch((error) => {
         console.log(error);
       });
-    }
+    },
+    getMember() {
+      console.log(this.getLoginMember.member)
+
+      http.get("/member/" + this.getLoginMember.member).then((response) => {
+        if (response.data.CODE == 200) {
+          this.$store
+              .dispatch("memberUpdate", { member: response.data.BODY })
+              .then(() => { this.$forceUpdate(); })
+              .catch(({ message }) => alert(message));
+        }
+      }).catch((error) => {
+        console.log(error);
+      });
+    },
   },
   created() {
+    if(this.getLoginMember != null){
+      this.info_mobile = this.getLoginMember.info_mobile;
+      this.info_email = this.getLoginMember.info_email;
+    }
   }
 }
 </script>
