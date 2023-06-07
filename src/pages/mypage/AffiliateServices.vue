b15d673962028754e1779e1d96081c84508459de
jiwon <jw.kim@itso.co.kr>
2023-06-07 15:15:56 +0900

    update 제휴 서비스 이미지 수정

--- src/pages/mypage/AffiliateServices.vue
+++ src/pages/mypage/AffiliateServices.vue
@@ -5,8 +5,10 @@
       <div class="service-page">
         <p class="title">제휴 서비스</p>
         <p class="sub-title">원하는 제휴 서비스가 있으시다면 gajahcompany@gmail.com으로 문의주세요.</p>
-        <div class="gray-box" v-for="service in this.serviceList">
-          <img :src="getImagePath(service.image, 0)" />
+        <!-- 1000X100 -->
+        <div class="gray-box" v-for="service in this.serviceList" :style="{ background: 'url(' + getImagePath(service.image, 0) + ') no-repeat' }">
+          <div v-html="service.content"></div>
+          <div class="link-box" @click="goToLink(service.link)">{{ service.title }}</div>
         </div>
       </div>
     </div>
