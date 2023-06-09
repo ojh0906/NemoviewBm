ea7149374ea1796914b5dbcf9c75aa7da794e187
jamie <woqja0@gmail.com>
2023-06-09 15:45:50 +0900

    jamie update 2023.06.09 ::: 환불 신청 추가

--- src/pages/mypage/PointCharge.vue
+++ src/pages/mypage/PointCharge.vue
@@ -5,6 +5,7 @@
       <div class="mypage-top">
         <span class="point">보유 포인트 : <strong>{{ this.addComma(this.getLoginMember != null ? this.getLoginMember.point:0) }}</strong></span>
         <router-link :to="{ name: 'PointCharge', query: {} }" class="charge" >충전하기</router-link>
+        <span class="charge refund" @click="this.refundPopup = true; this.point = 0; this.bank = ''; this.account = '';">환불하기</span>
       </div>
       <div class="mypage-content">
         <p class="page-title-lg">포인트 충전</p>
@@ -30,6 +31,35 @@
       </div>
     </div>
   </div>
+
+  <!-- 포인트 환불 팝업 -->
+  <section :class="this.refundPopup ? 'pop-up' : 'pop-up-off'" @click="clickSelect">
+    <article class="point-popup refund-pop" v-if="this.refundPopup">
+      <span class="close" @click="this.refundPopup = false">X</span>
+      <p class="title">포인트 환불</p>
+
+      <p class="sub-title">환불 금액</p>
+      <div class="input-wrap">
+        <input type="number" name="num" v-model="this.point" />
+        원
+      </div>
+      <p class="sub-title">은행</p>
+      <div class="input-wrap">
+        <input type="text" name="bank" v-model="this.bank" />
+      </div>
+      <p class="sub-title">계좌번호</p>
+      <div class="input-wrap">
+        <input type="text" name="account" v-model="this.account"/>
+      </div>
+      <p class="point1">보유 포인트 : <span> {{ addComma(this.getLoginMember.point) }}원</span></p>
+      <p class="point2">환불 후 금액 : <span> {{ addComma(this.getLoginMember.point - (this.point === '' ? 0 : this.point)) }}원</span></p>
+
+      <div class="btn">
+        <span class="charge" @click="saveRefund">환불신청</span>
+        <span class="cancle" @click="this.refundPopup = false;">취소</span>
+      </div>
+    </article>
+  </section>
 </template>
 
 
@@ -48,15 +78,76 @@ export default {
   props: [''],
   data() {
     return {
+      refundPopup: false,
+      point:0,
+      bank:'',
+      account:'',
     }
   },
   watch: {
 
   },
   methods: {
-
+    getMember() {
+      http.get("/member/" + this.getLoginMember.member).then((response) => {
+        if (response.data.CODE == 200) {
+          this.$store
+              .dispatch("memberUpdate", { member: response.data.BODY })
+              .then(() => { })
+              .catch(({ message }) => alert(message));
+        }
+      }).catch((error) => {
+        console.log(error);
+      });
+    },
+    clickSelect(event) {
+      if (event.target.classList.contains('pop-up')) {
+        console.log('popup')
+        this.refundPopup = false;
+      } else {
+        console.log('none')
+      }
+    },
+    saveRefund() {
+      if(this.point === '' || this.point === 0){
+        alert('환불금액을 입력해주세요.');
+        return;
+      }
+      if(this.bank === '' || this.account === ''){
+        alert('환불계좌정보를 입력해주세요.');
+        return;
+      }
+      if((this.getLoginMember.point - this.point) < 0){
+        alert('환불할 포인트가 부족합니다.');
+        return;
+      }
+      let param = {
+        type: 3,
+        point: this.point * (-1),
+        content: '사용자 환불',
+        bank: this.bank,
+        account: this.account
+      }
+      http.put("/member/" + this.getLoginMember.member + "/point", param).then((response) => {
+        if (response.data.CODE == 200) {
+          alert('환불 신청 되었습니다.');
+          this.refundPopup = false;
+          this.point = 0;
+          this.bank = '';
+          this.account = '';
+          this.getMember();
+        } else {
+          this.getMember();
+          alert('포인트 부족이거나 네트워크 이상으로 인한 환불 실패. 네트워크 담당자에게 문의하세요.');
+        }
+      }).catch((error) => {
+        console.log(error);
+        alert('환불 실패. 네트워크 담당자에게 문의하세요.');
+      });
+    }
   },
   created() {
+    this.getMember(); // 회원정보 갱신
   }
 }
 </script>
